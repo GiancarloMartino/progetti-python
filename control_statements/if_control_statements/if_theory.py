823eb317a45e49aa18f98ec95582698e9b430a1e
Giancarlo Martino <gianmart83@gmail.com>
2023-01-27 15:53:35 +0100

    rifinimento degli appunti

--- control_statements/if_control_statements/if_theory.py
+++ control_statements/if_control_statements/if_theory.py
@@ -34,3 +34,28 @@ print("Minimum: ", minimum)
 print("calcolato con le funzioni built-in max e min")
 print("Maximum: ", max(first, second))
 print("Minimum: ", min(first, second))
+
+#la forma più semplice di selezione è if statement. 
+#tale tipo di control statement è anche chiamata selezione ad una via, perché consiste di un una condizione 
+# e soltanto una singola sequenza di statement. Le la condizione è vera esegue tale sequenza, altrimenti procede allo 
+# step successivo, esterni all'if, eseguendo gli step rimanenti.
+#if <condizione>:
+#   <sequenza di istruzioni>
+
+#gli if statement vengono spesso utilizzati per prevenire l'esecuzione di alcuni comandi senza che siano nella giusta condizione.
+x = "qualcosa"
+if x < 0:
+    x= -x
+    
+#multiway if statements: quando bisogna testare varie condizioni e agire in base ad esse
+
+number = int(input("Inserisci un numero per una votazione: "))
+if number > 89:
+    lettera = 'A'
+elif number > 79:
+    lettera ='B'
+elif number > 69:
+    lettera ='C'
+else:
+    lettera = 'F'
+print("Il voto in lettere è ", lettera)
